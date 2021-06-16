cbbf3a3b589cbddf43b444ddee2c3115e6dc889c
Sinhika <sinhika@republicofnewhome.org>
2021-06-16 16:33:40 -0500

    added blocks to ModBlocks; classes will need to be changed later. Added large chunks to ModItems

--- src/main/java/mod/akkamaddi/goldenglitter/init/ModBlocks.java
+++ src/main/java/mod/akkamaddi/goldenglitter/init/ModBlocks.java
@@ -1,7 +1,14 @@
 package mod.akkamaddi.goldenglitter.init;
 
 import mod.akkamaddi.goldenglitter.GoldenGlitter;
+import mod.alexndr.simplecorelib.helpers.LightUtils;
 import net.minecraft.block.Block;
+import net.minecraft.block.RailBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -9,4 +16,28 @@ public final class ModBlocks
 {
     public static final DeferredRegister<Block> BLOCKS = 
             DeferredRegister.create(ForgeRegistries.BLOCKS, GoldenGlitter.MODID);
+    
+    // rail - TODO change registered class when coded.
+    public static final RegistryObject<RailBlock> red_gold_rail = BLOCKS.register("red_gold_rail",
+            () -> new RailBlock(Block.Properties.of(Material.DECORATION)
+                    .lightLevel(LightUtils.setFixedLight(15))
+                    .strength(0.7F).noCollission().sound(SoundType.METAL))); 
+            
+    // storage blocks - TODO change registered class when coded.
+    public static final RegistryObject<Block> rose_gold_block = BLOCKS.register("rose_gold_block",
+            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.COLOR_PINK)
+                    .strength(7.0F, 16.0F).sound(SoundType.METAL)
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+    public static final RegistryObject<Block> erubescent_gold_block = BLOCKS.register("erubescent_gold_block",
+            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.GOLD)
+                    .strength(9.0F, 14.0F).sound(SoundType.METAL).lightLevel(LightUtils.setFixedLight(4))
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+    public static final RegistryObject<Block> scarlatite_gold_block = BLOCKS.register("scarlatite_gold_block",
+            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.TERRACOTTA_ORANGE)
+                    .strength(11.0F, 18.0F).sound(SoundType.METAL).lightLevel(LightUtils.setFixedLight(15))
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+    public static final RegistryObject<Block> hephaestan_gold_block = BLOCKS.register("hephaestan_gold_block",
+            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE)
+                    .strength(18.0F, 24.0F).sound(SoundType.METAL)
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
 } // end class
--- src/main/java/mod/akkamaddi/goldenglitter/init/ModItems.java
+++ src/main/java/mod/akkamaddi/goldenglitter/init/ModItems.java
@@ -30,5 +30,13 @@ public final class ModItems
     public static final RegistryObject<Item> hephaestan_gold_nugget = ITEMS.register("hephaestan_gold_nugget", 
             ()-> new Item(new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP)));
 
-   
+    public static final RegistryObject<Item> large_rose_gold_chunk = ITEMS.register("large_rose_gold_chunk", 
+            ()-> new Item(new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_erubescent_gold_chunk = ITEMS.register("large_erubescent_gold_chunk", 
+            ()-> new Item(new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_scarlatite_gold_chunk = ITEMS.register("large_scarlatite_gold_chunk", 
+            ()-> new Item(new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_hephaestan_gold_chunk = ITEMS.register("large_hephaestan_gold_chunk", 
+            ()-> new Item(new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP)));
+  
 } // end class
