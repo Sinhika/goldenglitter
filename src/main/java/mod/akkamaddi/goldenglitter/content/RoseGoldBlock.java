cf5b0e0dcd60acf900fdc1e38e63920d837cb60d
Sinhika <sinhika@republicofnewhome.org>
2021-06-17 12:25:16 -0500

    finished defining RoseGoldBlock

---
+++ src/main/java/mod/akkamaddi/goldenglitter/content/RoseGoldBlock.java
@@ -0,0 +1,34 @@
+package mod.akkamaddi.goldenglitter.content;
+
+import java.util.Random;
+
+import mod.akkamaddi.goldenglitter.config.GoldenConfig;
+import mod.alexndr.simplecorelib.client.ClientUtils;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class RoseGoldBlock extends Block
+{
+    public RoseGoldBlock(Properties properties)
+    {
+        super(properties);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand)
+    {
+        if (worldIn.isClientSide && GoldenConfig.makeRoseHideous) 
+        {
+            super.animateTick(stateIn, worldIn, pos, rand);
+            double pv[] = ClientUtils.findBlockParticleVector(pos, rand);
+            worldIn.addParticle(ParticleTypes.HEART, pv[0], pv[1], pv[2], pv[3], pv[4], pv[5]);
+        }
+    } // end animateTick
+
+} // end class
--- src/main/java/mod/akkamaddi/goldenglitter/init/ModBlocks.java
+++ src/main/java/mod/akkamaddi/goldenglitter/init/ModBlocks.java
@@ -1,8 +1,10 @@
 package mod.akkamaddi.goldenglitter.init;
 
 import mod.akkamaddi.goldenglitter.GoldenGlitter;
+import mod.akkamaddi.goldenglitter.content.RoseGoldBlock;
 import mod.alexndr.simplecorelib.helpers.LightUtils;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.RailBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -24,20 +26,23 @@ public final class ModBlocks
                     .strength(0.7F).noCollission().sound(SoundType.METAL))); 
             
     // storage blocks - TODO change registered class when coded.
-    public static final RegistryObject<Block> rose_gold_block = BLOCKS.register("rose_gold_block",
-            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.COLOR_PINK)
+    public static final RegistryObject<RoseGoldBlock> rose_gold_block = BLOCKS.register("rose_gold_block",
+            () -> new RoseGoldBlock(Block.Properties.of(Material.METAL, MaterialColor.COLOR_PINK)
                     .strength(7.0F, 16.0F).sound(SoundType.METAL)
                     .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
     public static final RegistryObject<Block> erubescent_gold_block = BLOCKS.register("erubescent_gold_block",
             () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.GOLD)
                     .strength(9.0F, 14.0F).sound(SoundType.METAL).lightLevel(LightUtils.setFixedLight(4))
-                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops()
+                    .isRedstoneConductor(PropertyUtils::never))); 
     public static final RegistryObject<Block> scarlatite_gold_block = BLOCKS.register("scarlatite_gold_block",
             () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.TERRACOTTA_ORANGE)
                     .strength(11.0F, 18.0F).sound(SoundType.METAL).lightLevel(LightUtils.setFixedLight(15))
-                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops()
+                    .isRedstoneConductor(PropertyUtils::never))); 
     public static final RegistryObject<Block> hephaestan_gold_block = BLOCKS.register("hephaestan_gold_block",
             () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE)
                     .strength(18.0F, 24.0F).sound(SoundType.METAL)
-                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops())); 
+                    .harvestTool(ToolType.PICKAXE).harvestLevel(0).requiresCorrectToolForDrops()
+                    .isRedstoneConductor(PropertyUtils::never))); 
 } // end class
