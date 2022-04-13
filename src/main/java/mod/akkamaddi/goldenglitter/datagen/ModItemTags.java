400b544290a18be8b57f73e20618684466d92763
Sinhika <sinhika@republicofnewhome.org>
2022-04-12 20:13:30 -0500

    forgot to add storage block tags

--- src/main/java/mod/akkamaddi/goldenglitter/datagen/ModItemTags.java
+++ src/main/java/mod/akkamaddi/goldenglitter/datagen/ModItemTags.java
@@ -1,6 +1,7 @@
 package mod.akkamaddi.goldenglitter.datagen;
 
 import mod.akkamaddi.goldenglitter.GoldenGlitter;
+import mod.akkamaddi.goldenglitter.init.ModBlocks;
 import mod.akkamaddi.goldenglitter.init.ModItems;
 import mod.alexndr.simplecorelib.datagen.MiningItemTags;
 import mod.alexndr.simplecorelib.helpers.TagUtils;
@@ -22,8 +23,31 @@ public class ModItemTags extends MiningItemTags
         super.addTags();
         registerNuggetTags();
         registerIngotTags();
+        registerStorageBlockTags();
     }
 
+    /**
+     * Create standard forge tags for storage blocks.
+     */
+    private void registerStorageBlockTags()
+    {
+        this.tag(TagUtils.forgeTag("storage_blocks"))
+            .addTag(TagUtils.forgeTag("storage_blocks/rose_gold_block"))
+            .addTag(TagUtils.forgeTag("storage_blocks/erubescent_gold_block"))
+            .addTag(TagUtils.forgeTag("storage_blocks/scarlatite_gold_block"))
+            .addTag(TagUtils.forgeTag("storage_blocks/hephaestan_gold_block"));
+        
+        this.tag(TagUtils.forgeTag("storage_blocks/rose_gold_block"))
+            .add(ModBlocks.rose_gold_block.get().asItem());
+        this.tag(TagUtils.forgeTag("storage_blocks/erubescent_gold_block"))
+            .add(ModBlocks.erubescent_gold_block.get().asItem());
+        this.tag(TagUtils.forgeTag("storage_blocks/scarlatite_gold_block"))
+            .add(ModBlocks.scarlatite_gold_block.get().asItem());
+        this.tag(TagUtils.forgeTag("storage_blocks/hephaestan_gold_block"))
+            .add(ModBlocks.hephaestan_gold_block.get().asItem());
+    } // end registerStorageBlockTags
+
+    
     private void registerNuggetTags()
     {
         this.tag(TagUtils.forgeTag("nuggets"))
@@ -38,6 +62,7 @@ public class ModItemTags extends MiningItemTags
         this.tag(TagUtils.forgeTag("nuggets/hephaestan_gold")).add(ModItems.hephaestan_gold_nugget.get());
     } // end registerNuggetTags()
     
+    
     private void registerIngotTags()
     {
         this.tag(TagUtils.forgeTag("ingots"))
@@ -51,6 +76,5 @@ public class ModItemTags extends MiningItemTags
         this.tag(TagUtils.forgeTag("ingots/scarlatite_gold")).add(ModItems.scarlatite_gold_ingot.get());
         this.tag(TagUtils.forgeTag("ingots/hephaestan_gold")).add(ModItems.hephaestan_gold_ingot.get());
     } // end registerIngotTags()
-
     
 } // end class
